6eb11efac68ec59e0ca8edd9cd63d45615dc3044
mohamad <aghi.mohamad@gmail.com>
2022-04-12 00:42:05 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(act = "roller-skate") {
+    return "This Saturday, I want to "+ act +"!"
+}
+
+let mondayWork = function(act = "go to the office") {
+    return "This Monday, I will "+act+"."
+}
+
+function saturdayFun(act = 'roller-skate') {
+    return "This Saturday, I want to "+act+"!"
+}
+function mondayWork(act = 'go to the office') {
+    return "This Monday, I want to "+act+"!"
+}
+
+function wrapAdjective(s = "*"){
+    return function(p = "special"){
+        return "You are " + s + p + s +"!"
+    }
+}
